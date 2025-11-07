04acd42547276b36a3e41a6589addef848dbfa4c
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-11-07 19:19:39 -0300

    feat: primeiro commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite um número");
+
+            int numUser = Convert.ToInt32(Console.ReadLine());
+
+            if(numUser % 2 == 0)
+            {
+                System.Console.WriteLine("Par!");
+            }
+            else
+            {
+                System.Console.WriteLine("Impar!");
+            }
+        }
+    }
+}
