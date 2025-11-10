ab20a98822f6ebf6039dbd2101e593f277b616a0
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-11-10 15:10:00 -0300

    addd: condicionais em CSharp

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 
             int numUser = Convert.ToInt32(Console.ReadLine());
 
-            if(numUser % 2 == 0)
+            if (numUser % 2 == 0)
             {
                 System.Console.WriteLine("Par!");
             }
@@ -16,6 +16,21 @@
             {
                 System.Console.WriteLine("Impar!");
             }
+
+            System.Console.WriteLine("Digite a hora atual:");
+            int horaAtual = Convert.ToInt32(Console.ReadLine());
+
+            if (horaAtual < 12)
+            {
+                System.Console.WriteLine("Bom dia!");
+            } else if ((horaAtual == 12) || (horaAtual <= 18))
+            {
+                System.Console.WriteLine("Boa tarde!");
+            } else if (horaAtual > 18)
+            {
+                System.Console.WriteLine("Boa noite!");
+            }
+
         }
     }
 }
